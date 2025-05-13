2c051f25b7e20db0c27e8fdaa813a54967b1c2a9
niera <71599424+nieraraqel@users.noreply.github.com>
2025-05-13 10:32:17 +0400

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <map>
-#include <vector>
+#include <deque>
 #include <algorithm>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-const int TANK_WIDTH = 50;
-const int TANK_HEIGHT = 50;
-const int BULLET_WIDTH = 10;
-const int BULLET_HEIGHT = 10;
+const int TANK_WIDTH = 64;
+const int TANK_HEIGHT = 64;
+const int BULLET_WIDTH = 24;
+const int BULLET_HEIGHT = 16;
 const int BULLET_SPEED = 5;
 
 struct Tank {
@@ -24,16 +25,22 @@ struct Bullet {
     int speedX;
 };
 
-void DrawTank(SDL_Renderer* renderer, const Tank& tank) {
-    SDL_Rect rect = {tank.x, tank.y, TANK_WIDTH, TANK_HEIGHT};
-    SDL_SetRenderDrawColor(renderer, tank.color.r, tank.color.g, tank.color.b, 255);
-    SDL_RenderFillRect(renderer, &rect);
+void DrawTank(SDL_Renderer* renderer, SDL_Texture* texture, const Tank& tank, int index) {
+    SDL_Rect srcRect = {index * 16, 0, 16, 16};
+    SDL_Rect dstRect = {tank.x, tank.y, TANK_WIDTH, TANK_HEIGHT};
+    SDL_RenderCopy(renderer, texture, &srcRect, &dstRect);
+    // SDL_Rect rect = {tank.x, tank.y, TANK_WIDTH, TANK_HEIGHT};
+    // SDL_SetRenderDrawColor(renderer, tank.color.r, tank.color.g, tank.color.b, 255);
+    // SDL_RenderFillRect(renderer, &rect);
 }
 
-void DrawBullet(SDL_Renderer* renderer, const Bullet& bullet) {
-    SDL_Rect rect = {bullet.x, bullet.y, BULLET_WIDTH, BULLET_HEIGHT};
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &rect);
+void DrawBullet(SDL_Renderer* renderer, SDL_Texture* texture, const Bullet& bullet) {
+    SDL_Rect srcRect = {32, 0, 6, 4};
+    SDL_Rect dstRect = {bullet.x, bullet.y, BULLET_WIDTH, BULLET_HEIGHT};
+    SDL_RenderCopy(renderer, texture, &srcRect, &dstRect);
+    // SDL_Rect rect = {bullet.x, bullet.y, BULLET_WIDTH, BULLET_HEIGHT};
+    // SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    // SDL_RenderFillRect(renderer, &rect);
 }
 
 void DrawText(SDL_Renderer* renderer, TTF_Font* font, const std::string& text,
@@ -62,24 +69,24 @@ void HandleEvents(bool& running, std::map<SDL_Keycode, bool>& keys) {
 }
 
 void ProcessInput(std::map<SDL_Keycode, bool>& keys, Tank& player, Tank& enemy,
-                  std::vector<Bullet>& bulletsPlayer, std::vector<Bullet>& bulletsEnemy) {
+                  std::deque<Bullet>& bulletsPlayer, std::deque<Bullet>& bulletsEnemy) {
     if (keys[SDLK_w]) {
-        if (player.y != 0) {
+        if (player.y > 0) {
             player.y -= 5;
         }
     }
     if (keys[SDLK_s]) {
-        if (player.y != 550) {
+        if (player.y < 550) {
             player.y += 5;
         }
     }
     if (keys[SDLK_UP]) {
-        if (enemy.y != 0) {
+        if (enemy.y > 0) {
             enemy.y -= 5;
         }
     }
     if (keys[SDLK_DOWN]) {
-        if (enemy.y != 550) {
+        if (enemy.y < 550) {
             enemy.y += 5;
         }
     }
@@ -121,11 +128,10 @@ void ProcessInput(std::map<SDL_Keycode, bool>& keys, Tank& player, Tank& enemy,
     }
 }
 
-void UpdateBullets(std::vector<Bullet>& bullets, Tank& target) {
+void UpdateBullets(std::deque<Bullet>& bullets, Tank& target) {
     for (auto& b : bullets) {
         b.x += b.speedX;
     }
-
     bullets.erase(
         std::remove_if(bullets.begin(), bullets.end(),
             [&](const Bullet& b) {
@@ -137,8 +143,7 @@ void UpdateBullets(std::vector<Bullet>& bullets, Tank& target) {
                     return true;
                 }
                 return (b.x < 0 || b.x > SCREEN_WIDTH);
-            }),
-        bullets.end()
+            }), bullets.end()
     );
 }
 
@@ -163,13 +168,24 @@ int main() {
         std::cerr << "Failed to load font\n";
         return 1;
     }
+    // ------ SDL IMAGE
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        std::cerr << "IMG_Init Error: " << IMG_GetError() << "\n";
+        return 1;
+    }
+    
+    SDL_Texture* texture = IMG_LoadTexture(renderer, "../sprites.png");
+    if (!texture) {
+        std::cerr << "Failed to load texture: " << IMG_GetError() << "\n";
+        return 1;
+    }
 
     // ------ TANKS
     Tank player = {50, 100, 10, {0, 255, 0}};
     Tank enemy = {SCREEN_WIDTH - TANK_WIDTH - 50, 100, 10, {255, 0, 0}};
 
-    std::vector<Bullet> bulletsPlayer;
-    std::vector<Bullet> bulletsEnemy;
+    std::deque<Bullet> bulletsPlayer;
+    std::deque<Bullet> bulletsEnemy;
     std::map<SDL_Keycode, bool> keys;
 
     bool running = true;
@@ -184,14 +200,14 @@ int main() {
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        DrawTank(renderer, player);
-        DrawTank(renderer, enemy);
+        DrawTank(renderer, texture, player, 0);
+        DrawTank(renderer, texture, enemy, 1);
 
         for (auto& b : bulletsPlayer) {
-            DrawBullet(renderer, b);
+            DrawBullet(renderer, texture, b);
         }
         for (auto& b : bulletsEnemy) {
-            DrawBullet(renderer, b);
+            DrawBullet(renderer, texture, b);
         }
 
         SDL_Color white = {255, 255, 255};
@@ -205,6 +221,8 @@ int main() {
     TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    SDL_DestroyTexture(texture);
+    IMG_Quit();
     TTF_Quit();
     SDL_Quit();
     return 0;
